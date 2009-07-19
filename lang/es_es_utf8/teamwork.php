64e33b3f916fd958c9140ffcc3ffc5917d028032
Javier Aranda <internet@javierav.com>
2009-07-19 14:41:09 +0200

    añadida la estructura básica de la localizacion i18n

---
+++ lang/es_es_utf8/teamwork.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Cadenas de internacionalizacion español de España es_ES UTF8
+ *
+ * Este archivo contiene las cadenas usadas en el módulo para su
+ * internacionalizacion
+ *
+ * @author Javier Aranda <internet@javierav.com>
+ * @version 0.1
+ * @package teamwork
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero GPL 3
+ */
+
+//
+///usadas por moodle y de obligada inclusión
+//
+
+$string['modulename'] = 'teamwork';
+$string['modulenameplural'] = 'teamworks';
+
+
+
+//
+/// usadas por nuestro módulo
+//
+
+
+?>
