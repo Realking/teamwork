098cf2ec818bdf8fcfb33eb3c10e63a0072934c8
Javier Aranda <internet@javierav.com>
2009-07-21 12:53:36 +0200

    modificaciones en el archivo de librerias requeridas por moodle (ya se puede crear, editar y eliminar una instancia) y creación del archivo de funciones locales del módulo.

--- lib.php
+++ lib.php
@@ -1,20 +1,15 @@
 <?php
 /**
- * Funciones específicas para el módulo
+ * Funciones de integración con moodle
  *
  * Este archivo contiene las funciones básicas requeridas por moodle para su
- * correcto funcionamiento más las definidas específicamente para realizar
- * tareas en el módulo
+ * correcto funcionamiento e interacción
  *
  * @author Javier Aranda <internet@javierav.com>
  * @version 0.1
  * @package teamwork
  * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero GPL 3
  */
-
-//
-/// Funciones requeridas por moodle
-//
 
 /**
  * Añade una nueva instancia de teamwork en la base de datos
@@ -23,11 +18,78 @@
  * @return integer id de la nueva instancia del teamwork
  */
 function teamwork_add_instance($teamwork)
-{
-    // TODO implementar el codigo de add_instance
-
-    var_dump($teamwork);
+{	
+	//añadir los valores por defecto
+	if($teamwork->allowselecteval == '0')
+	{
+		$teamwork->selectevalmin = 0;
+		$teamwork->selectevalmax = 0;
+		$teamwork->selectteammax = 0;
+	}
+	
+	return insert_record('teamwork', $teamwork);
 }
 
+/**
+ * Actualiza una instancia de teamwork en la base de datos
+ *
+ * @param object $teamwork datos enviados por el formulario
+ * @return boolean status code de la operación
+ */
+function teamwork_update_instance($teamwork)
+{	
+	//añadir los valores por defecto
+	if($teamwork->allowselecteval == '0')
+	{
+		$teamwork->selectevalmin = 0;
+		$teamwork->selectevalmax = 0;
+		$teamwork->selectteammax = 0;
+	}
+	
+	$teamwork->id = $teamwork->instance;
+	
+	return update_record('teamwork', $teamwork);
+}
 
+/**
+ * Elimina una instancia de teamwork y todos sus datos
+ * 
+ * @param integer $id id de la instancia a eliminar
+ * @return boolean status code de la operación
+ */
+function teamwork_delete_instance($id)
+{
+	//obtenemos los datos de la instancia de teamwork
+	if (! $teamwork = get_record("teamwork", "id", "$id"))
+	{
+        return false;
+    }
+	
+	//por defecto el resultado de la eliminación es positivo
+    $result = true;
+	
+	//borrar tablas con datos referentes a esta instancia
+	// TODO
+	
+	//en ultima instancia borramos los datos del teamwork
+	if (! delete_records("teamwork", "id", "$teamwork->id"))
+	{
+        $result = false;
+    }
+	
+	//devolver el resultado de la operación
+	return $result;
+}
+
+/**
+ * Realiza comprobaciones periodicas acorde al cron de moodle
+ * 
+ * - Enviar recordatorios por email a los alumnos
+ * 
+ * @return void
+ */
+function teamwork_cron()
+{
+	return true;
+}
 ?>
---
+++ locallib.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Funciones específicas del modulo teamwork
+ *
+ * Este archivo contiene las funciones específicas usadas en el módulo
+ *
+ * @author Javier Aranda <internet@javierav.com>
+ * @version 0.1
+ * @package teamwork
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero GPL 3
+ */
+
+?>
