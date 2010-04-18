3ac6c1915b39174864fd1c4728208918aedd19e5
Javier Aranda <internet@javierav.com>
2010-04-18 20:05:23 +0200

    Borrado de toda la información de la base de datos al eliminar una instancia

--- lib.php
+++ lib.php
@@ -59,25 +59,82 @@ function teamwork_update_instance($teamwork)
  */
 function teamwork_delete_instance($id)
 {
-	//obtenemos los datos de la instancia de teamwork
-	if (! $teamwork = get_record("teamwork", "id", "$id"))
+	// Obtenemos los datos de la instancia de teamwork
+	if(! $teamwork = get_record('teamwork', 'id', $id))
 	{
-        return false;
+    return false;
+  }
+	
+	// Por defecto el resultado de la eliminación es positivo
+  $result = true;
+
+  //
+  // Plantillas
+  //
+
+  // Si las plantillas que usa no son usadas en otra instancia de este curso, las borramos
+  if($tpls = get_records('teamwork_tplinstances', 'teamworkid', $teamwork->id))
+  {
+    foreach($tpls as $tpl)
+    {
+      // Comprobamos si esta plantilla se está usando en otra instancia
+      if( !count_records('teamwork_tplinstances', 'templateid', $tpl->templateid))
+      {
+        // No se está usando, la podemos borrar
+        // Borramos los items de esa plantilla
+        $result = $result && delete_records('teamwork_items', 'templateid', $tpl->templateid);
+
+        // Borramos la plantilla en si
+        $result =  $result && delete_records('teamwork_templates', 'id', $tpl->templateid);
+      }
     }
-	
-	//por defecto el resultado de la eliminación es positivo
-    $result = true;
-	
-	//borrar tablas con datos referentes a esta instancia
-	// TODO
-	
-	//en ultima instancia borramos los datos del teamwork
-	if (! delete_records("teamwork", "id", "$teamwork->id"))
-	{
-        $result = false;
+  }
+
+  // Borramos las intancias de plantillas a este teamwork
+  $result = $result && delete_records('teamwork_tplinstances', 'teamworkid', $teamwork->id);
+
+  //
+  /// Equipos
+  //
+
+  // Obtenemos la lista de equipos de esta instancia de teamwork
+  if($teams =  get_records('teamwork_teams', 'teamworkid', $teamwork->id))
+  {
+    foreach($teams as $team)
+    {
+      // Eliminamos los usuarios del equipo
+      $result = $result && delete_records('teamwork_users_teams', 'teamid', $team->id);
     }
+  }
+
+  // Borramos los equipos
+  $result = $result && delete_records('teamwork_teams', 'teamworkid', $teamwork->id);
+
+  //
+  /// Evaluaciones
+  //
+
+  // Obtenemos la lista de evaluaciones
+  if($evals =  get_records('teamwork_evals', 'teamworkid', $teamwork->id))
+  {
+    foreach($evals as $eval)
+    {
+      // Eliminar las evaluaciones
+      $result = $result && delete_records('teamwork_eval_items', 'evalid', $eval->id);
+    }
+  }
+
+  // Borramos las evaluaciones
+  $result = $result && delete_records('teamwork_evals', 'teamworkid', $teamwork->id);
+
+  //
+  /// Instancia
+  //
+
+  // Borramos la instancia del teamwork
+  $result = $result && delete_records('teamwork', 'id', $teamwork->id);
 	
-	//devolver el resultado de la operación
+	// Devolver el resultado de la operación
 	return $result;
 }
 
